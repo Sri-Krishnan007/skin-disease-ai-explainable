28ca01ff2372ca68f794c9bca1126e5edb734223
Sri-Krishnan007 <srikrish2705guru@gmail.com>
2026-04-02 12:41:41 +0530

    UPDATE NUMPY

--- app.py
+++ app.py
@@ -338,4 +338,4 @@ if __name__ == "__main__":
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
 
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=10000, debug=False)
